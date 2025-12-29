eb705df0f35f17766c83d45949470e1e9e819909
dah4k <84741290+dah4k@users.noreply.github.com>
2025-12-29 00:09:46 -0500

    Add LoadEmbeddedFile()
    Reference: https://khalidabuhakmeh.com/how-to-use-embedded-resources-in-dotnet

--- Program.cs
+++ Program.cs
@@ -1,14 +1,29 @@
-﻿// Copyright 2025 dah4k
+// Copyright 2025 dah4k
 // SPDX-License-Identifier: EPL-2.0
 
-// See https://aka.ms/new-console-template for more information
+using System.IO;
+using System.Reflection;
+using System.Text;
 using System;
 
 namespace RcHello;
 class Program
 {
+    static private string LoadEmbeddedFile()
+    {
+        // https://khalidabuhakmeh.com/how-to-use-embedded-resources-in-dotnet
+        var info = Assembly.GetExecutingAssembly().GetName();
+        var name = info.Name;
+        using var stream = Assembly
+            .GetExecutingAssembly()
+            .GetManifestResourceStream($"{name}.Files.greetings.txt")!;
+        using var streamReader = new StreamReader(stream, Encoding.UTF8);
+        return streamReader.ReadToEnd();
+    }
+
     static void Main(string[] args)
     {
         Console.WriteLine("Hello World!");
+        Console.Write(LoadEmbeddedFile());
     }
 }
