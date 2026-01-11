ded682dd3838ce2270e6ea0235cf3754cb2dcd4b
dah4k <84741290+dah4k@users.noreply.github.com>
2026-01-11 13:10:17 -0500

    Rename namespace from RcHello to Csharp.RcHello

--- Program.cs
+++ Program.cs
@@ -6,7 +6,7 @@ using System.Reflection;
 using System.Text;
 using System;
 
-namespace RcHello;
+namespace Csharp.RcHello;
 class Program
 {
     static private string LoadEmbeddedFile(string pathname)
